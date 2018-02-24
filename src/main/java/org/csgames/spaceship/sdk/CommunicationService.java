dad4190884e9bc56e8fa7e7217dd28a7601e0f3f
Ian Létourneau <letourneau.ian@gmail.com>
2018-02-24 15:11:15 -0500

    Communication service should fetch water from the supplier

--- src/main/java/org/csgames/spaceship/sdk/CommunicationService.java
+++ src/main/java/org/csgames/spaceship/sdk/CommunicationService.java
@@ -1,5 +1,9 @@
 package org.csgames.spaceship.sdk;
 
+import static org.csgames.spaceship.sdk.EventType.FISH_FETCHED;
+import static org.csgames.spaceship.sdk.EventType.MOVED_TO;
+import static org.csgames.spaceship.sdk.EventType.WATER_FETCHED;
+
 public class CommunicationService {
 
   private final Headquarters headquarters;
@@ -10,19 +14,27 @@ public class CommunicationService {
 
   public void moveTo(String target, Direction direction, int distance) {
     String movement = String.format("%s,%d", direction, distance);
-    Event event = new Event(EventType.MOVED_TO, target, movement);
-    headquarters.recordEvent(event);
+    recordEvent(MOVED_TO, target, movement);
   }
 
-  public void fetchFishFrom(String consumer, String supplier) {
+  public void fetchFishFrom(String consumer, String supplier, int fishCount) {
+    String fishFetchedFrom = String.format("%s,%d", supplier, fishCount);
+    recordEvent(FISH_FETCHED, consumer, fishFetchedFrom);
   }
 
-  public void fetchWaterFrom(String consumer, String supplier) {
+  public void fetchWaterFrom(String consumer, String supplier, int waterInLiter) {
+    String waterFetchedFrom = String.format("%s,%d", supplier, waterInLiter);
+    recordEvent(WATER_FETCHED, consumer, waterFetchedFrom);
   }
 
   public void catchFish(String target, double longitude, double latitude) {
   }
 
   public void refillWater(String target, double longitude, double latitude) {
+  }
+
+  private void recordEvent(EventType eventType, String target, String payload) {
+    Event event = new Event(eventType, target, payload);
+    headquarters.recordEvent(event);
   }
 }
--- src/test/java/org/csgames/spaceship/sdk/CommunicationServiceTest.java
+++ src/test/java/org/csgames/spaceship/sdk/CommunicationServiceTest.java
@@ -9,8 +9,13 @@ import static org.mockito.Mockito.verify;
 public class CommunicationServiceTest {
 
   private static final String THE_TARGET = "team-03";
+  private static final String THE_SUPPLIER = "team-11";
+
   private static final int THE_DISTANCE = 3174;
   private static final Direction THE_DIRECTION = Direction.NORTH;
+  private static final int THE_FISH_COUNT = 47;
+  private static final int THE_WATER_AMOUNT = 15;
+
   private Headquarters headquarters;
   private CommunicationService communicationService;
 
@@ -26,6 +31,22 @@ public class CommunicationServiceTest {
 
     String expectedMovement = String.format("%s,%d", THE_DIRECTION, THE_DISTANCE);
     verifyEventRecorded(EventType.MOVED_TO, THE_TARGET, expectedMovement);
+  }
+
+  @Test
+  public void itShouldRecordThatTheTargetFetchedFishFromTheSupplier() {
+    communicationService.fetchFishFrom(THE_TARGET, THE_SUPPLIER, THE_FISH_COUNT);
+
+    String expectedFishFetched = String.format("%s,%d", THE_SUPPLIER, THE_FISH_COUNT);
+    verifyEventRecorded(EventType.FISH_FETCHED, THE_TARGET, expectedFishFetched);
+  }
+
+  @Test
+  public void itShouldRecordThatTheTargetFetchedWaterFromTheSupplier() {
+    communicationService.fetchWaterFrom(THE_TARGET, THE_SUPPLIER, THE_WATER_AMOUNT);
+
+    String expectedWaterFetched = String.format("%s,%d", THE_SUPPLIER, THE_WATER_AMOUNT);
+    verifyEventRecorded(EventType.WATER_FETCHED, THE_TARGET, expectedWaterFetched);
   }
 
   private void verifyEventRecorded(EventType eventType, String target, String payload) {
