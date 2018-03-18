8cb4bb68f476c8ae8e70d40f01907dbca9ec3246
Ian Létourneau <letourneau.ian@gmail.com>
2018-03-18 11:29:25 -0400

    Hardcode first feature in acceptance serivce

--- src/main/java/org/csgames/spaceship/sdk/accept/userstory/UserStoryRepositoryJsonFile.java
+++ src/main/java/org/csgames/spaceship/sdk/accept/userstory/UserStoryRepositoryJsonFile.java
@@ -24,7 +24,7 @@ public class UserStoryRepositoryJsonFile implements UserStoryRepository {
   @Override
   public List<UserStory> findAll() {
 
-    try (InputStream inputStream = getClass().getResourceAsStream("/user_stories/send_supplies_to_research_team.json")) {
+    try (InputStream inputStream = getClass().getResourceAsStream("/user_stories/F1-track_away_team_location.json")) {
       InputStreamReader reader = new InputStreamReader(inputStream);
       UserStory userStory = gson.fromJson(reader, UserStory.class);
       return asList(userStory);
