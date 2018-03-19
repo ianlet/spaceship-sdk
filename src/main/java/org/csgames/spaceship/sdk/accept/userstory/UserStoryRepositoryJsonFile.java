b6b31605ea5e4987fa30d37e2b47bc6b1d5ceb5e
Ian Létourneau <letourneau.ian@gmail.com>
2018-03-18 21:47:16 -0400

    List all user stories from resources folder (#14)

--- src/main/java/org/csgames/spaceship/sdk/accept/userstory/UserStoryRepositoryJsonFile.java
+++ src/main/java/org/csgames/spaceship/sdk/accept/userstory/UserStoryRepositoryJsonFile.java
@@ -6,12 +6,25 @@ import com.google.gson.GsonBuilder;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
-import static java.util.Arrays.asList;
+import static java.nio.file.Files.isRegularFile;
 import static java.util.Collections.emptyList;
+import static java.util.stream.Collectors.toList;
 
 public class UserStoryRepositoryJsonFile implements UserStoryRepository {
+
+  private static final String USER_STORIES_DIR = "/user_stories";
 
   private final Gson gson;
 
@@ -23,13 +36,53 @@ public class UserStoryRepositoryJsonFile implements UserStoryRepository {
 
   @Override
   public List<UserStory> findAll() {
-
-    try (InputStream inputStream = getClass().getResourceAsStream("/user_stories/F1-track_away_team_location.json")) {
-      InputStreamReader reader = new InputStreamReader(inputStream);
-      UserStory userStory = gson.fromJson(reader, UserStory.class);
-      return asList(userStory);
-    } catch (IOException e) {
+    try {
+      List<Path> userStoryPaths = listUserStoryPaths();
+      return userStoryPaths.stream()
+        .map(this::toInputStream)
+        .filter(Objects::nonNull)
+        .map(this::toReader)
+        .map(this::toUserStory)
+        .collect(toList());
+    } catch (IOException | URISyntaxException e) {
       return emptyList();
     }
+  }
+
+  private List<Path> listUserStoryPaths() throws URISyntaxException, IOException {
+    Path userStoryDir = getUserStoryDir();
+    return Files.walk(userStoryDir, 1)
+      .filter(path -> isRegularFile(path))
+      .collect(toList());
+  }
+
+  private Path getUserStoryDir() throws IOException, URISyntaxException {
+    URI uri = getClass().getResource(USER_STORIES_DIR).toURI();
+    if (isJar(uri)) {
+      FileSystem fileSystem = FileSystems.newFileSystem(uri, Collections.emptyMap());
+      return fileSystem.getPath(USER_STORIES_DIR);
+    } else {
+      return Paths.get(uri);
+    }
+  }
+
+  private boolean isJar(URI uri) {
+    return uri.getScheme().equals("jar");
+  }
+
+  private InputStream toInputStream(Path path) {
+    try {
+      return Files.newInputStream(path);
+    } catch (IOException e) {
+      return null;
+    }
+  }
+
+  private InputStreamReader toReader(InputStream inputStream) {
+    return new InputStreamReader(inputStream);
+  }
+
+  private UserStory toUserStory(Reader reader) {
+    return gson.fromJson(reader, UserStory.class);
   }
 }
